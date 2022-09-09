67db7ae0725578d409220a6ab6f761c9dd9471b5
Ochoja <danielochoja@yahoo.com>
2022-09-09 04:23:42 +0000

    updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,10 @@
 
 int main(void)
 {
-printf("%d\n", sizeof(int));
-printf("%d\n", sizeof(char));
-printf("%d\n", sizeof(float));
-printf("%d\n", sizeof(double));
-printf("%d\n", sizeof(long));
-printf("%d\n", sizeof(short));
+printf("Size of a char: %d byte(s)", sizeof(char));
+printf("Size of a int: %d byte(s)", sizeof(int));
+printf("Size of a long int: %d byte(s)", sizeof(long));
+printf("Size of a long long int: %d byte(s)", sizeof(long long int));
+printf("Size of a float: %d byte(s)", sizeof(float));
 return (0);
 }
