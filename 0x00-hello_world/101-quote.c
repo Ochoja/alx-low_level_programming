c176bb7652fbca6516f5c595bed81f687b15e353
Ochoja <danielochoja@yahoo.com>
2022-09-09 04:53:08 +0000

    updated

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Return: 0 to indicate end of program
+ */
+int main(void)
+{
+write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (0);
+}
