951dbc20a9e876220077e9ec187bb58494ee9b49
Ochoja <danielochoja@yahoo.com>
2022-09-09 04:14:44 +0000

    updated

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - print size of various types
+ *Return: 0 to end program
+ */
+
+int main(void)
+{
+printf("%d\n", sizeof(int));
+printf("%d\n", sizeof(char));
+printf("%d\n", sizeof(float));
+printf("%d\n", sizeof(double));
+printf("%d\n", sizeof(long));
+printf("%d\n", sizeof(short));
+return (0);
+}
