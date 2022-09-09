4601e3fc0634f03ea51ea5e07009f0c4113ee47b
Ochoja <danielochoja@yahoo.com>
2022-09-09 03:52:38 +0000

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+/**
+ *main - string using puts() function
+ *Return: 0 to indicate end of program
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
