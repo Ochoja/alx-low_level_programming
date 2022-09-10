34c4156a76d6a68c691e9900127c2628ba6c135d
Ochoja <danielochoja@yahoo.com>
2022-09-10 03:26:56 +0000

    updated

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,13 +9,13 @@ int main(void)
 	int num = 0;
 	char letter = 'a';
 
-	for (num = '0'; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
 	}
 
 	while (letter <= 'f')
-        {
+	{
 		putchar(letter);
 		letter++;
 	}
