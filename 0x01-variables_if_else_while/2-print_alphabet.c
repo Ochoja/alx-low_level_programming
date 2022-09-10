d9c21e44b5b2a333f5df76d790f9e7e8eb626187
Ochoja <danielochoja@yahoo.com>
2022-09-10 01:59:59 +0000

    updated

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function execution
+ * Return: 0 to terminate program
+ */
+
+int main(void)
+{
+	char alp = a;
+
+	while(alp <= 'z')
+	{
+		putchar(i);
+		alp++;
+	}
+	putchar("\n");
+
+	return (0);
+}
