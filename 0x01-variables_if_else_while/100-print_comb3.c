b2c2c5a1990b9adfed1a9255d5a6dbfe6347897a
Ochoja <danielochoja@yahoo.com>
2022-09-10 04:16:32 +0000

    updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 /**
  * main - main function
+ * Description: Program prints all possible combination
+ * of two numbers
  * Return: 0 to terminate
  */
 
 int main(void)
 {
+	/*counter and number variables*/
 	int i;
 	int j;
 
 	for (i = '0'; i <= '8'; i++)
 	{
-		for (j = i+1 ; j <= '9'; j++)
+		for (j = i + 1 ; j <= '9'; j++)
 		{
 			putchar(i);
 			putchar(j);
