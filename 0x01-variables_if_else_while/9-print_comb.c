8d28ff19cb7df96b9d579a8bb7e80402a9c0687f
Ochoja <danielochoja@yahoo.com>
2022-09-10 03:35:20 +0000

    updated

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 to terminate function
+ */
+
+int main(void)
+{
+	/*loop variables*/
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
