9beb5961962d4e2012dcbac255ac90eda9b9191f
Ochoja <danielochoja@yahoo.com>
2022-09-10 02:28:12 +0000

    added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 to terminate function
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	/*print numbers from 0-9*/
+	while (i <= 9)
+	{
+		printf("%d", number);
+		number++;
+	}
+
+	printf("\n");
+}
