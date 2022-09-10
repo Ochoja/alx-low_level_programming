6d1bfdb227c85d865c438194211a062a59acaee9
Ochoja <danielochoja@yahoo.com>
2022-09-10 03:47:29 +0000

    updated

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,14 @@ int main(void)
 
 	for (number = '0'; number <= '9' ; number++)
 	{
-		if (number != 9){
+		if (number < 9)
+		{
 			putchar(number);
 			putchar(',');
 			putchar(' ');
 		}
-		else {
+		else
+		{
 			putchar(number);
 		}
 	}
