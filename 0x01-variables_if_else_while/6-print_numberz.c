060844b5f4b4be05a6eb1c5b7473991d8457d1a5
Ochoja <danielochoja@yahoo.com>
2022-09-10 02:35:28 +0000

    added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main function execution
+ * Return: 0 terminate program
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	while (number >= 9)
+	{
+		putchar(number);
+		number++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
