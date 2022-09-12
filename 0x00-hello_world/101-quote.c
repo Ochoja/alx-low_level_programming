f491de1d50f7d6554545d5a607b7d528e0f7edcf
Ochoja <danielochoja@yahoo.com>
2022-09-12 16:20:21 +0000

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  *main - entry point
- *Return: 0 to indicate end of program
+ *Return: 1 on success.
  */
 int main(void)
 {
-write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
