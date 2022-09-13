88594b51a70700798231d96f0dcb6351c48c20ee
Ochoja <danielochoja@yahoo.com>
2022-09-13 14:04:56 +0000

    added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 to terminate
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
