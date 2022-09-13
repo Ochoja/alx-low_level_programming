c818e692c315b1882492f34d4e29c7f3133118e4
Ochoja <danielochoja@yahoo.com>
2022-09-13 22:21:42 +0000

    updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,10 @@
 #include "main.h"
 
 /**
- * main - main function execution
  * print_alphabet - function prints all alphabets
  * Return: Always 0
  */
 
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 
 void print_alphabet(void)
 {
