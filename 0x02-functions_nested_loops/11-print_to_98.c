c665c26d7cf0042e25bfc91c7c9fed438488d787
Ochoja <danielochoja@yahoo.com>
2022-09-14 04:42:43 +0000

    task 11 done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_to_98 - print from 9 t0 98
+ * @n: argument
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+
+			printf("%d, ", n--);
+
+		printf("%d\n", n);
+	}
+
+
+
+	else
+	{
+		while (n < 98)
+
+			printf("%d, ", n++);
+
+		printf("%d\n", n);
+	}
+}
