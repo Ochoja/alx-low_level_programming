e9d64d0acf66c7db425a0a92a2a71ff401342283
Ochoja <danielochoja@yahoo.com>
2022-09-14 02:39:37 +0000

    updated

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _islower(int c);
+ * _islower(int c) - check for lower letter
  * Return: 1 if true, 0 otherwise
  */
 
 int _islower(int c)
 {
-	if ( c >= 'a' && c <= 'z' )
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
