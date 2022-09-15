706861859531196270d7c753ad8f7da37c6cf0b6
Ochoja <danielochoja@yahoo.com>
2022-09-15 21:20:30 +0000

    updated

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase character
+ * @c: argument variable
+ * Return: 1 if true, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
