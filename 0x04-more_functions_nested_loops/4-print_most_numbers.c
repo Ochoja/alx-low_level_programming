29cd4a0515cea61332396ccee2ef79f046dd317a
Ochoja <danielochoja@yahoo.com>
2022-09-15 21:53:46 +0000

    added 4-put_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print number from 0-9 excluding 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	/*iterator varible*/
+	int i;
+
+	for (i = '0'; i <= 9; i++)
+	{
+		if ( i == '0' || i == '9')
+			continue;
+
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
