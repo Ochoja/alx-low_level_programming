5669a5b3c6e9a69779a421c466129fa6a8954639
Ochoja <danielochoja@yahoo.com>
2022-09-15 22:42:50 +0000

    updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,16 +17,21 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j > 9)
-			{
+			/**reset number printed**/
+			if (j == 10)
 				num = '0';
+
+			/**input 1 before number printed**/
+			if (j > 9)
 				_putchar('1');
-			}
+
 
 			_putchar(num);
 			num++;
 		}
+
+		_putchar('\n');
+		num = '0';
 	}
 
-	_putchar('\n');
 }
