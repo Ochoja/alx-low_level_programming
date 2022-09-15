4f590ae30eb95b014e13013fb33af0c15b982384
Ochoja <danielochoja@yahoo.com>
2022-09-15 23:44:19 +0000

    updated

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,13 +15,17 @@ void print_diagonal(int n)
 	if (n <= 0)
 		_putchar('\n');
 
-	for (i = 1; i <= n; i++)
+	else
 	{
-		for (j = 1; j <= i; j++)
+		for (i = 1; i <= n; i++)
 		{
-			_putchar(' ');
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar('\\');
-		_putchar('\n');
 	}
+
 }
