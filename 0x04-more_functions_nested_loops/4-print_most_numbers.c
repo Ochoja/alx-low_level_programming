a8f477b7aaf5d2d16f514ffa7a6bad96491a64eb
Ochoja <danielochoja@yahoo.com>
2022-09-15 21:55:57 +0000

    updated

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,9 @@ void print_most_numbers(void)
 	/*iterator varible*/
 	int i;
 
-	for (i = '0'; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if ( i == '0' || i == '9')
+		if ( i == '2' || i == '4')
 			continue;
 
 		_putchar(i);
