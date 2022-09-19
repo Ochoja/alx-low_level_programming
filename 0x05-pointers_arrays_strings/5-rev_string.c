f15581d0f18eabc4caf2133d626d7280c00e423a
Ochoja <danielochoja@yahoo.com>
2022-09-19 23:01:31 +0000

    task #5 code added

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: pointer to string
+ */
+
+void rev_string(char *s)
+{
+	char tempString*;
+	int total = 0, i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		total++;
+
+	for (i = 0; (total - 1) >= 0; i++)
+	{
+		tempString[i] = s[total - 1];
+		total--;
+	}
+
+	for (i = 0; tempString[i] != '\0'; i++)
+		s[i] = tempString[i];
+}
