1c24e1d1b3acac9ec6047cff59ace154b139d6dd
Ochoja <danielochoja@yahoo.com>
2022-09-19 17:17:37 +0000

    updated

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - print a string followed by a new line
+ * @str - character pointer (pointer to either char or char array)
+ */
+
+void _puts(char *str)
+{
+	int i;
+	char c;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		c = *(str + i);
+	        _putchar(c);
+	}
+
+	_putchar('\n');
+}
