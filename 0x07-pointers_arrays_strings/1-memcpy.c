a1c0b8327705e5ce24e7d72a13147cfe0f8ab01e
Ochoja <danielochoja@yahoo.com>
2022-09-27 04:38:31 +0000

    updated

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,8 +11,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int i = 0;
 
-	for (i = 0; i <= n; i++)
-		dest[0] = src[0];
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 
 	return (dest);
 }
