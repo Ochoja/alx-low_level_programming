b61af604610c531497321ba140f913a4c4404af4
Ochoja <danielochoja@yahoo.com>
2022-09-28 02:58:38 +0000

    updated

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,13 +14,12 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (accept[j++])
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-				return (&s[j]);
+				return (&s[i]);
 
 		}
-		j = 0;
 	}
 
 	return (NULL);
