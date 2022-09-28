80cb44bebdf5f0791690141a5ede694234ec0b5e
Ochoja <danielochoja@yahoo.com>
2022-09-28 02:02:48 +0000

    updated

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - get the length of a prefix substring
+ * @s: string
+ * @accept: substring
+ * Return: length of prefix substring
+ */
+
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int length = 0;
+	int i = 0;
+	int j = 0;
+
+	while ( (s[i] >= 65 && s[i] < 92) || (s[i] >= 97 && s[i] < 123) )
+		{
+			while (accept[j++])
+			{
+				if (s[i] == accept[j])
+					length++;
+			}
+			j = 0;
+			i++;
+		}
+
+	return (length);
+}
