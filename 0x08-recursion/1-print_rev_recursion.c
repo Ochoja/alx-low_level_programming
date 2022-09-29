0007e9f97142780cff3893ced723d1675e8b009b
Ochoja <danielochoja@yahoo.com>
2022-09-29 03:33:36 +0000

    added 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string argument
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int n = 0;
+
+	if (s[n + 1] != '\0')
+		_print_rev_recursion(&s[n+1]);
+
+	_putchar(s[n]);
+}
