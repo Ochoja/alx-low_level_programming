172711a0175e7426c74f7b4826c29d5d168ec56e
Ochoja <danielochoja@yahoo.com>
2022-09-29 03:09:22 +0000

    modified 0-puts..

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,13 @@ void _puts_recursion(char *s)
 {
 	int n = 0;
 	if (s[n] == '\0')
+	{
+		_putchar('\n');
 		return;
+	}
 	else
 	{
 		_putchar(s[n]);
-		_puts_recursion(s[n + 1]);
+		_puts_recursion(&s[n + 1]);
 	}
 }
