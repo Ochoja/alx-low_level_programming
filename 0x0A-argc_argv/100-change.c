a0c28adc830f8a3d23485fe30ec0d6c4cbc83fb8
Ochoja <danielochoja@yahoo.com>
2022-10-01 03:27:54 +0000

    updated

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,7 +4,7 @@
  * main - coins to make change for an amount of money
  * @argc: argument counter
  * @argv: array of pointer to strings
- * Return: 0 
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
