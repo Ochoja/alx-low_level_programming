e4a0e8d3c5201f32dacff4ce4e0ca9fa6e1b99c7
Ochoja <danielochoja@yahoo.com>
2022-10-17 12:30:50 +0000

    updated

--- main.c
+++ main.c
@@ -4,6 +4,10 @@ void modif_my_char_var(char *cc, char ccc)
 {
 	*cc = 'o';
 	ccc = 'l';
+
+	printf("\n\n");
+	printf("The value of pointer 'cc' is %p\n", cc);
+	printf("The value of the address %p is %c\n", cc, *cc);
 }
 
 
@@ -18,7 +22,6 @@ int main(void)
 	printf("The value of 'c' is %c\n", c);
 	printf("The address of 'c' is %p\n", &c);
 	printf("The value of 'p' is %p\n", p);
-	
 
 	modif_my_char_var(p, c);
 
