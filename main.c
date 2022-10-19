82afcac91b688c4c9e9b6d571fb80979471ad468
Ochoja <danielochoja@yahoo.com>
2022-10-19 20:07:43 +0000

    updated

--- main.c
+++ main.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 
+struct data{
+	int sum;
+	char *ptr;
+};
+
 int main(void)
 {
-	int a[3];
+	struct data Name;
+	Name.sum = 50;
+	Name.ptr = "Holberton";
 
-	*a = 8;
-	*(a + 1) = 10;
-	*(a + 2) = 12;
+	printf("%s is %d\n", Name.ptr, Name.sum);
 
-	printf("*a : %d\n", *a);
-	printf("*(a + 1) : %d\n", *(a + 1));
-	printf("*(a + 2) : %d\n", *(a + 2));
-
-	printf("---------------\n");
-	printf("Value of a : %d\n", a);
-	printf("Value of (a+1): %d\n", a + 1);
-	printf("Value of &a+1: %d\n", &a + 1);
-
-
-	return (0);
+	return 0;
 }
