5f53c8a6943e26dc40698559b7408d1b475503b4
Ochoja <danielochoja@yahoo.com>
2022-10-28 16:15:48 +0000

    added task 2 code

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,13 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * add_node - adds node to beginning of a list
+ * @head: pointer to pointer to list
+ * @str: string to be added
+ * Return: pointer to list
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+}
