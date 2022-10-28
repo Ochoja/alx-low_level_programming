07176411e0c6b61d5d477b2a741b56ca51ef6555
Ochoja <danielochoja@yahoo.com>
2022-10-28 12:57:40 +0000

    updated

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "lists.h"
 
 /**
@@ -10,7 +11,7 @@ size_t list_len(const list_t *h)
 {
 	unsigned int length = 0;
 
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		length++;
 		h = h->next;
