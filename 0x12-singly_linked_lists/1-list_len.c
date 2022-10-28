d017b067cc7f2a7b09b7ba8e41c9542ede59b633
Ochoja <danielochoja@yahoo.com>
2022-10-28 12:53:47 +0000

    updated

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - function returns length of list
+ * @h: pointer to list
+ * Return: length of list
+ */
+
+size_t list_len(const list_t *h)
+{
+	unsigned int length = 0;
+
+	while (h->next != NULL)
+	{
+		length++;
+		h = h->next;
+	}
+
+	return (length);
+}
