a74138019e7776e1ae2cdee1a03990c8da9a0081
Daniel Ochoja <danielochoja@yahoo.com>
2022-11-03 17:07:44 +0000

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-void before_main() __attribute__((constructor));
+void before_main(void) __attribute__((constructor));
 
-void before_main()
+void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n I bore my house upon my back!\n");
 }
