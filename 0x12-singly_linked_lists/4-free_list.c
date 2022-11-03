aacebd2a27a1613fdcd4fb96decb6e3479aee476
Daniel Ochoja <danielochoja@yahoo.com>
2022-11-03 16:31:08 +0000

    updated

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,6 +2,11 @@
 #include <stddef.h>
 #include "lists.h"
 
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to head node
+ * Return: void
+ */
 void free_list(list_t *head)
 {
 	free(head);
