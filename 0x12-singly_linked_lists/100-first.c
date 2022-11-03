cc14eaa44b7be291235ae543fe18853014868e25
Daniel Ochoja <danielochoja@yahoo.com>
2022-11-03 17:10:50 +0000

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,6 +2,9 @@
 
 void before_main(void) __attribute__((constructor));
 
+/**
+ * before_main - function runs before main function
+ */
 void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
