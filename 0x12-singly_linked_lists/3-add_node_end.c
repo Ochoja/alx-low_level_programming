d509c6c20e643a89949d60c540f27a346f75a315
Daniel Ochoja <danielochoja@yahoo.com>
2022-11-03 08:26:23 +0000

    updated

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,7 +2,6 @@
 #include <stddef.h>
 #include <string.h>
 #include "lists.h"
-
 /**
  * add_node_end - add node to end of list
  * @head: pointer to head node
@@ -13,7 +12,7 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	unsigned int len = 0;
 	int i = 0;
-	list_t *lastnode;
+	list_t *lastnode = *head;
 	list_t *node = (list_t *)malloc(sizeof(list_t));
 
 	if (node == NULL)
@@ -27,27 +26,20 @@ list_t *add_node_end(list_t **head, const char *str)
 	node->str = strdup(str);
 	node->next = NULL;
 
-	lastnode = traverse(head);
-	lastnode->next = node;
-
-	return (node);
-}
-
-
-/**
- * traverse - move to last node
- * @head: pointer to head node
- * Return: pointer to last node
- */
-list_t *traverse(list_t **head)
-{
-	list_t *ptr = *head;
-
-	if (ptr == NULL || ptr->next == NULL)
-		return (ptr);
+	if (*head == NULL)
+		*head = (list_t *)node;
 	else
 	{
-		while (ptr->next != NULL)
-			ptr = ptr->next;
+		if (lastnode->next == NULL)
+			lastnode->next = (list_t *)node;
+		else
+		{
+			while (lastnode->next != NULL)
+				lastnode = lastnode->next;
+
+			lastnode->next = node;
+		}
 	}
+
+	return (node);
 }
