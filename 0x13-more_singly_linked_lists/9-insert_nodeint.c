f483419c2d479ddef9074c5253455e3b0636633d
Daniel Ochoja <danielochoja@yahoo.com>
2022-11-07 11:11:02 +0000

    updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,6 +24,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		ptr = ptr->next;
 		i++;
+
+		if (!ptr || !ptr->next)
+			return (NULL);
 	}
 
 	node->next = ptr->next;
