1a9e83c8061f15d04372bc3ef4118b947c5efce6
Daniel Ochoja <danielochoja@yahoo.com>
2022-11-07 10:51:25 +0000

    updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,9 +29,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (i < idx - 1)
 	{
 		*ptr = (*ptr)->next;
-
-		if (!(*ptr)->next)
-			return NULL;
 	}
 
 	node->next = (*ptr)->next;
