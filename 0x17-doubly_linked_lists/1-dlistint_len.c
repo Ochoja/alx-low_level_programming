465b66dffdf91faf6362e8015138d8e92ce9019b
Daniel Ochoja <danielochoja@yahoo.com>
2022-12-15 06:44:35 +0000

    updated

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,12 +3,12 @@
 /**
  * dlistint_len - returns number of elements in list
  * @h: pointer to head node
- * @Return: length of list
+ * Return: length of list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t length = 0;
-	const dlistint_t hold = h;
+	const dlistint_t *hold = h;
 
 	while (hold != NULL)
 	{
