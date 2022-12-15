dbd0cd145fe3239fa1b1293044c0d369fcba3def
Daniel Ochoja <danielochoja@yahoo.com>
2022-12-15 06:47:15 +0000

    updated

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,9 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <stddef.h>
+#include <string.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -21,5 +22,6 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif
